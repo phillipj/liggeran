ebbcc928dc332ee088bf7f44bed3ae263ab0b466
leftieFriele <leftie.friele@gmail.com>
2012-06-14 14:29:31 +0200

    echo script added

--- schema.js
+++ schema.js
@@ -6,5 +6,6 @@ var pg = require('pg').native
 client = new pg.Client(connectionString);
 client.connect();
 //query = client.query('CREATE TABLE reporters (email varchar(200), date date, lastLogin date)');
-query = client.query('CREATE TABLE verifications (token varchar(200), requesteddate varchar(20), ip varchar(100))');
+query = client.query('drop table verifications');
+query = client.query('CREATE TABLE verifications (token varchar(200), requesteddate varchar(30), ip varchar(100))');
 query.on('end', function() { client.end(); });
