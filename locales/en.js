d780b87c8de7b3c71f6d9530825e30c6652b6ef2
leftieFriele <leftie.friele@gmail.com>
2014-06-29 21:02:24 +0200

    first shot at locale switch #18

---
+++ locales/en.js
@@ -0,0 +1,7 @@
+{
+    "registration_header": "Add wage information",
+    "registration_email": "Email",
+    "registration_salary": "Base salary",
+    "registration_years_working": "Years working",
+    "registration_register_button": "See how you are doing"
+}
