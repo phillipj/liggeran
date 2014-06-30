b56d3536402c279f603c96a86dd70e8b5637546a
leftieFriele <leftie.friele@gmail.com>
2014-06-30 21:48:18 +0200

    splitting about into a route

--- routes/about.js
+++ routes/about.js
@@ -1,3 +1,14 @@
-module.exports = function(request, reply){
-	
-}
+var i18n = new (require('i18n-2'))({
+    locales: ['no', 'en'],
+    cookie: 'locale'
+});
+
+module.exports = {
+	handler: function(request, reply){
+        reply.view('about', {
+            page: {
+                title: i18n.__('about_page_title')
+            }
+        });
+    }
+};
