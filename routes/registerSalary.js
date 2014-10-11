6c46295a2e07e905fc6434f4904b52236b8a63b5
Jari Bakken <jari.bakken@gmail.com>
2014-10-11 17:56:34 +0200

    fix whitespace + form action

--- routes/registerSalary.js
+++ routes/registerSalary.js
@@ -1,15 +1,15 @@
 var joi = require('joi');
 
 var registerSalaryConfig = {
-	handler: function(request, reply){
-		request.log(['registering salary']);
+    handler: function(request, reply){
+        request.log(['registering salary']);
         console.log('handler...');
         reply('success');
     },
     validate: {
-    	query: {
-    		workEmail: joi.string().min(5).max(100)
-    	}
+        query: {
+            workEmail: joi.string().min(5).max(100)
+        }
     }
 };
 module.exports = registerSalaryConfig;
